8ef0b0a568df5b985316a150d724c3f36741deae
NG Ivan Blanco <gabriel.blanco@nacerglobal.com.mx>
2025-09-05 13:41:00 -0600

    Ajuste Credenciales BD

--- app/logic/conn.php
+++ app/logic/conn.php
@@ -1,7 +1,7 @@
 <?php
 $ServerName = "localhost";
 $Username = "root";
-$Password = "xTNhJSP0)Ai}";
+$Password = "";
 $NameBD = "ser_rh";
 $mysqli=new mysqli($ServerName, $Username, $Password, $NameBD); 
 
